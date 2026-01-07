0748c646c3ea1fce1fb89386c431a2e5d312aee3
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-08 00:51:30 +0530

    first commit with prateek

---
+++ arraysL1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// sum of digits
+
+int main(){
+   
+    //driver function
+
+    //i/p - 699
+
+    // sum , digits , num
+
+    /*
+       divisor )      dividend    ( quotient
+  
+        ------------   =>  Dividend = quo * divisor + remainder
+        remainder
+    */
+
+    /*
+    num = 699
+    digits = num % 10 = 699 % 10 = 9 
+    num = num / 10 = 699 / 10 = 69
+    digits = num % 10 = 69 % 10 = 9
+    num = num / 10 = 69 / 10 = 6
+    digits = num % 10 = 6 % 10 = 0
+
+    sum = 0 
+    sum + 9 + 9 + 6 = 24  ...    
+    */
+  
+
+
+    return 0;
+}
